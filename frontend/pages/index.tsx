57d4091c8039f2ddcb406675c4f7437dab8c3d1b
Ofek Asido <asido.ofek@gmail.com>
2022-05-27 01:59:57 +0300

    Add ScheduleItem type

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -11,13 +11,13 @@ const Home = () => {
         <h1 className="font-medium text-2xl my-5">מה תרצו לקבוע?</h1>
 
         <div className="flex flex-wrap justify-center gap-2 mt-4">
-          <ScheduleItem label="לאכול" icon="GiKnifeFork" />
-          <ScheduleItem label="פגישה" icon="FcBusiness" />
-          <ScheduleItem label="פעילות" icon="GiBowlingStrike" />
-          <ScheduleItem label="ביחד" icon="MdOutlineGroups" />
-          <ScheduleItem label="מרוחק" icon="GiLaptop" />
-          <ScheduleItem label="מסיבה" icon="GiPartyPopper" />
-          <ScheduleItem label="אחר" icon="GoCalendar" />
+          <ScheduleItem type="food" label="לאכול" icon="GiKnifeFork" />
+          <ScheduleItem type="business" label="פגישה" icon="FcBusiness" />
+          <ScheduleItem type="activity" label="פעילות" icon="GiBowlingStrike" />
+          <ScheduleItem type="together" label="ביחד" icon="MdOutlineGroups" />
+          <ScheduleItem type="remote" label="מרוחק" icon="GiLaptop" />
+          <ScheduleItem type="party" label="מסיבה" icon="GiPartyPopper" />
+          <ScheduleItem type="other" label="אחר" icon="GoCalendar" />
         </div>
       </div>
     </>
