cfa2c5338a64bf38f1f889467000be1b24897759
Ofek Asido <asido.ofek@gmail.com>
2022-05-27 01:56:02 +0300

    Add Link to a create event page

--- frontend/modules/ScheduleItem/ScheduleItem.tsx
+++ frontend/modules/ScheduleItem/ScheduleItem.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { GiKnifeFork } from 'react-icons/gi';
 import { FcBusiness } from 'react-icons/fc';
 import { GiBowlingStrike } from 'react-icons/gi';
@@ -7,6 +8,7 @@ import { GiPartyPopper } from 'react-icons/gi';
 import { GoCalendar } from 'react-icons/go';
 
 interface ScheduleItemProps {
+  type: string;
   label: string;
   icon: string;
 }
@@ -35,12 +37,14 @@ const ScheduleItem = (item: ScheduleItemProps) => {
 
   return (
     <>
-      <div className="grid group hover:cursor-pointer">
-        <span className="text-5xl mx-auto border-2 rounded-lg p-6 group-hover:bg-green-500 group-hover:text-white transition">
-          {getIcon(item.icon)}
-        </span>
-        <h1 className="font-medium text-xl mt-1">{item.label}</h1>
-      </div>
+      <Link href={{ pathname: `/event/create`, query: { type: item.type } }}>
+        <div className="grid group hover:cursor-pointer">
+          <span className="text-5xl mx-auto border-2 rounded-lg p-6 group-hover:bg-green-500 group-hover:text-white transition">
+            {getIcon(item.icon)}
+          </span>
+          <h3 className="font-medium text-xl mt-1 select-none">{item.label}</h3>
+        </div>
+      </Link>
     </>
   );
 };
