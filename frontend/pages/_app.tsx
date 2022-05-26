5724132e780fe6e34c30ae24ea6fad1d55715ab5
Ofek Asido <asido.ofek@gmail.com>
2022-05-27 02:00:44 +0300

    Fix RTL

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { AppShell } from '@mantine/core';
 import Navbar from './../modules/navbar/index';
 import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { MantineProvider } from '@mantine/core';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <AppShell padding={0}>
         {/* <Navbar links={[{ link: '/meet/create', label: 'יצירת פגישה' }]} /> */}
-        <Component {...pageProps} />
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ dir: 'rtl' }}
+          emotionOptions={{ key: 'rtl', stylisPlugins: [rtlPlugin] }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
       </AppShell>
       {/* links: { link: string; label: string }[]; */}
     </>
--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,13 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { ServerStyles, createStylesServer } from '@mantine/next';
 
-export default function Document() {
-  return (
-    <Html dir="rtl">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const stylesServer = createStylesServer({ key: 'rtl', stylisPlugins: [rtlPlugin] });
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <ServerStyles html={initialProps.html} server={stylesServer} />
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html dir="rtl" lang={this.props.locale}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
