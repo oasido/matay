6596d20ca333807e37d18e1c40fa36aff0995cb5
Ofek Asido <asido.ofek@gmail.com>
2022-05-28 03:30:16 +0300

    (wip) Add WhereComponent

---
+++ frontend/modules/WhereComponent.tsx
@@ -0,0 +1,67 @@
+import { Input, MultiSelect, Text } from '@mantine/core';
+import { useDebouncedValue } from '@mantine/hooks';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { GiFalloutShelter } from 'react-icons/gi';
+
+const WhenComponent = (props) => {
+  const { location, setLocation } = props;
+  const [results, setResults] = useState([]);
+  let searchResults = [];
+  const [debouncedLocation] = useDebouncedValue(location, 300);
+
+  const handleLocationInput = (e) => {
+    setLocation(e);
+  };
+
+  const handleSearch = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://trueway-places.p.rapidapi.com/FindPlaceByText',
+      params: { text: debouncedLocation, language: 'he' },
+      headers: {
+        'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com',
+        'X-RapidAPI-Key': '5d184f4bebmsh6418304ef432175p111c0ejsn7614b9380f74',
+      },
+    };
+
+    axios.request(options).then((response) => {
+      response.data.results &&
+        response.data.results.forEach((result) => {
+          searchResults.push(result.address);
+        });
+      setResults(searchResults);
+    });
+    // .catch(function (error) {
+    //   console.error(error);
+    // });
+  };
+
+  useEffect(() => {
+    location && handleSearch();
+  }, [debouncedLocation]);
+
+  console.log(results);
+
+  return (
+    <>
+      <div className="mb-24">
+        <MultiSelect
+          size="lg"
+          classNames={{ input: 'font-medium' }}
+          placeholder="(אופציונאלי) מיקום"
+          searchable
+          maxSelectedValues={1}
+          value={location}
+          onSearchChange={(e) => {
+            handleLocationInput(e);
+          }}
+          nothingFound="לא נמצאו תוצאות"
+          data={results}
+        />
+      </div>
+    </>
+  );
+};
+
+export default WhenComponent;
