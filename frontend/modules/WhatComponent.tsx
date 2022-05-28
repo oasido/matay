7e7292243dec2f676e352f634c2a906fca9cd808
Ofek Asido <asido.ofek@gmail.com>
2022-05-28 03:29:44 +0300

    Set suggestions based on router.query

--- frontend/modules/WhatComponent.tsx
+++ frontend/modules/WhatComponent.tsx
@@ -18,6 +18,52 @@ const WhatComponent = (props) => {
     required,
     setRequired,
   } = props;
+  const router = useRouter();
+  const type = router.query.type;
+
+  const suggestions = {
+    food: [
+      'ארוחה',
+      'ארוחת בוקר',
+      'ארוחת צהריים',
+      'ארוחת ערב',
+      'סושי',
+      'פיצה',
+      'המבורגר',
+      'על האש',
+      'תה',
+      'קפה',
+      'סלט',
+    ],
+    business: ['פגישה', 'התייעצות', 'אסיפה', 'פגישת צוות', 'הערכה', 'פרויקט'],
+    activity: ['פעילות', 'סרט', 'הופעה', 'ספורט', 'באולינג', 'אסקייפרום', 'קונצרט'],
+    together: ['ביחד', 'טיול משפחתי', 'אוכל', 'אירוע משפחתי', 'דייט'],
+    remote: ['פגישת זום', 'דיסקורד', 'שיחת וידאו', 'שיחת פלאפון'],
+    party: ['מסיבה', 'יום הולדת', 'מסיבת הפתעה'],
+    other: ['אחר', 'מפגש'],
+  };
+
+  const getSuggestions = (type) => {
+    switch (type) {
+      case 'food':
+        return suggestions.food;
+      case 'business':
+        return suggestions.business;
+      case 'activity':
+        return suggestions.activity;
+      case 'together':
+        return suggestions.together;
+      case 'remote':
+        return suggestions.remote;
+      case 'party':
+        return suggestions.party;
+      case 'other':
+        return suggestions.other;
+      default:
+        return [];
+    }
+  };
+  const currentSuggestions = getSuggestions(type);
 
   return (
     <>
