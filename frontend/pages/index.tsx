22dff76067df46ec2a9a5602df8d069c044211bd
Ofek Asido <asido.ofek@gmail.com>
2022-05-30 00:28:52 +0300

    Replace text with heading component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,8 +6,9 @@ import Heading from './../modules/Heading';
 const Home = () => {
   return (
     <>
+      <ProgressBar step={-1} type={'start'} />
       <div className="my-10">
-        <h1 className="text-center font-bold text-xl">matay</h1>
+        <Heading />
       </div>
       <div className="mx-auto text-center">
         <h1 className="font-medium text-2xl my-5">מה תרצו לקבוע?</h1>
