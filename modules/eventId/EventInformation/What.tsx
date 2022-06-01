958e6141aba1b3cb3850168cd0ba0dec6806d5b9
Ofek Asido <asido.ofek@gmail.com>
2022-06-02 02:08:06 +0300

    Remove redundant import and fragment

--- modules/eventId/EventInformation/What.tsx
+++ modules/eventId/EventInformation/What.tsx
@@ -1,17 +1,13 @@
-import { SimpleGrid } from '@mantine/core';
-
 const What = ({ eventData }) => {
   return (
-    <>
-      <div className="mt-10 mb-5">
-        <h2 className="text-2xl font-medium text-right">מה?</h2>
-        {eventData.description === '' ? (
-          <p className="text-gray-500 text-right">ללא תיאור.</p>
-        ) : (
-          <p className="text-gray-500 text-right">{eventData.description}</p>
-        )}
-      </div>
-    </>
+    <div className="mt-8">
+      <h2 className="text-2xl font-medium text-right">מה?</h2>
+      {eventData.description === '' ? (
+        <p className="text-gray-500 text-right">ללא תיאור.</p>
+      ) : (
+        <p className="text-gray-500 text-right">{eventData.description}</p>
+      )}
+    </div>
   );
 };
 
