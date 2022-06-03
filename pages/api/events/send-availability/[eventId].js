df4fe087420f3dca8e4775887aa55ef2feff07c4
Ofek Asido <asido.ofek@gmail.com>
2022-06-04 01:42:40 +0300

    Check if event exists

--- pages/api/events/send-availability/[eventId].js
+++ pages/api/events/send-availability/[eventId].js
@@ -5,6 +5,8 @@ const registerParticipant = async (req, res) => {
   try {
     const { eventId } = req.query;
     const { availability, name, email } = req.body;
+    const event = await Event.findById(eventId);
+    event === null && res.send('Event not found.');
   } catch (error) {
     console.error(error);
     res.send(error.msg);
