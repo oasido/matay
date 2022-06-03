106a6fb47b9f8ecaf730285fd7171dc9fcc6ba93
Ofek Asido <asido.ofek@gmail.com>
2022-06-04 01:42:24 +0300

    Get both the eventId and the info that came within the fetch call

--- pages/api/events/send-availability/[eventId].js
+++ pages/api/events/send-availability/[eventId].js
@@ -3,6 +3,8 @@ import Participant from '../../../../models/Participant';
 
 const registerParticipant = async (req, res) => {
   try {
+    const { eventId } = req.query;
+    const { availability, name, email } = req.body;
   } catch (error) {
     console.error(error);
     res.send(error.msg);
