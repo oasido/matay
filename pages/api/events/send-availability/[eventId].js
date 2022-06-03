12594313157bad5047de2c2d18fa418e76d1f77b
Ofek Asido <asido.ofek@gmail.com>
2022-06-04 01:41:58 +0300

    Add skeleton function for handling an API call (not only POST for now)

--- pages/api/events/send-availability/[eventId].js
+++ pages/api/events/send-availability/[eventId].js
@@ -1,2 +1,12 @@
 import Event from '../../../../models/Event';
 import Participant from '../../../../models/Participant';
+
+const registerParticipant = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.send(error.msg);
+  }
+};
+
+export default registerParticipant;
