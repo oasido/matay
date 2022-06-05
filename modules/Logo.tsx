23ed13a1d576f5a1d7023718cc98d1730b8cf4b7
Ofek Asido <asido.ofek@gmail.com>
2022-06-05 19:20:08 +0300

    Pass Href

--- modules/Logo.tsx
+++ modules/Logo.tsx
@@ -5,7 +5,7 @@ import MatayLogo from './../public/matay-black.svg';
 const Logo = () => {
   // add logic to replace this svg with its dark theme version.
   return (
-    <Link href="/">
+    <Link href="/" passHref>
       <div className="my-5 flex justify-center">
         <Image className="hover:cursor-pointer" src={MatayLogo} width={250} height={85} />
       </div>
