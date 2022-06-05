3c4d3ff6bb0d3e9ff02a37530ee1de7bffac0c5e
Ofek Asido <asido.ofek@gmail.com>
2022-06-05 19:49:39 +0300

    Add success/fail pages after attempting to create an event

---
+++ pages/event/create/fail.tsx
@@ -0,0 +1,22 @@
+import { Input, Button } from '@mantine/core';
+import Logo from '../../../modules/Logo';
+import Image from 'next/image';
+import FailImg from '../../../public/fail.png';
+
+const Fail = () => {
+  return (
+    <>
+      <Logo />
+      <div className="mx-auto text-center">
+        <h1 className="font-medium text-2xl mt-5 mb-2">אוו אוו</h1>
+        <h3 className="font-medium text-lg mb-5 text-gray-500">אירעה שגיאה, נסה שוב מאוחר יותר.</h3>
+
+        <div className="mx-auto w-10/12 md:w-9/12 lg:w7/12 xl:w-6/12">
+          <Image src={FailImg} width={400} height={400} layout="fixed" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Fail;
---
+++ pages/event/create/success.tsx
@@ -0,0 +1,54 @@
+import { Input, Button } from '@mantine/core';
+import Logo from '../../../modules/Logo';
+import { useClipboard } from '@mantine/hooks';
+import { useEffect, useState } from 'react';
+
+const Success = () => {
+  const URL = process.env.NEXT_PUBLIC_URL;
+
+  const [submitStatus, setSubmitStatus] = useState(null);
+  const [eventId, setEventId] = useState(null);
+  useEffect(() => {
+    setSubmitStatus(localStorage.getItem('submitStatus'));
+    setEventId(localStorage.getItem('eventId'));
+  }, []);
+
+  const clipboard = useClipboard({ timeout: 500 });
+
+  return (
+    <>
+      <Logo />
+      <div className="mx-auto text-center">
+        <h1 className="font-medium text-2xl mt-5 mb-2">האירוע נוצר בהצלחה!</h1>
+        <h3 className="font-medium text-lg mb-5 text-gray-500">העתיקו את הקישור למטה.</h3>
+
+        <div className="mx-auto w-10/12 md:w-9/12 lg:w7/12 xl:w-6/12">
+          {eventId !== null ? (
+            <>
+              <Input
+                size="lg"
+                classNames={{ input: 'font-medium' }}
+                value={`${URL}/event/${JSON.parse(eventId)}`}
+                readOnly
+              />
+              <Button
+                color={clipboard.copied ? 'teal' : 'blue'}
+                onClick={() => clipboard.copy(`${URL}/event/${JSON.parse(eventId)}`)}
+                size="lg"
+                className="mt-5"
+              >
+                {clipboard.copied ? 'הועתק' : 'העתק'}
+              </Button>
+            </>
+          ) : (
+            <h4 className="font-medium text-lg">
+              משום מה אין לנו אפשרות להציג את הקישור שנוצר, האם אתם במצב incognito?
+            </h4>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Success;
