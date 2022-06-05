2a1158ced127a8e1c2999f22785a909ff23173ba
Ofek Asido <asido.ofek@gmail.com>
2022-06-05 19:29:59 +0300

    Add event info on submit in localStorage

--- pages/event/create/index.tsx
+++ pages/event/create/index.tsx
@@ -1,3 +1,20 @@
+import useLocalStorage from '../../../hooks/useLocalStorage';
+
+  const [submitStatus, setSubmitStatus] = useLocalStorage('submitStatus', null);
+  const [eventId, setEventId] = useLocalStorage('eventId', null);
+
+  const handleSubmit = async () => {
+    const { values } = form;
+    const response = await axios.post('/api/events/create', { type, values });
+    if (response.status === 200) {
+      setSubmitStatus(200);
+      setEventId(response.data.eventId);
+      router.push('/event/create/success');
+    } else {
+      setSubmitStatus(response.status);
+      router.push('/event/create/fail');
+    }
+  };
         <StepsButton
           label={step >= 0 && step < 3 ? 'הבא' : 'סיום'}
           color="default"
