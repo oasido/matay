759590602809de47f8f0e67b32611d7994057809
Ofek Asido <asido.ofek@gmail.com>
2022-06-06 18:17:12 +0300

    Add alert if previous schedules were found in local storage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
 import ScheduleItem from './../modules/ScheduleItem';
 import ProgressBar from './../modules/ProgressBar';
 import Logo from '../modules/Logo';
+import { Alert } from '@mantine/core';
+import { AiFillInfoCircle } from 'react-icons/ai';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 const Home = () => {
+  const [eventId, setEventId] = useState(null);
+
+  useEffect(() => {
+    setEventId(JSON.parse(JSON.parse(localStorage.getItem('eventId'))));
+  }, []);
+
   return (
     <>
       <ProgressBar step={-1} type={'start'} />
       <Logo />
+
       <div className="mx-auto text-center">
         <h1 className="font-medium text-2xl my-5">מה תרצו לקבוע?</h1>
 
@@ -20,6 +31,22 @@ const Home = () => {
           <ScheduleItem type="other" label="אחר" icon="GoCalendar" />
         </div>
       </div>
+      {eventId !== null && eventId.length > 0 && (
+        <div className="flex flex-wrap justify-center mt-7">
+          <Alert
+            className="w-96"
+            icon={<AiFillInfoCircle />}
+            title="פגישות נוספות"
+            color="blue"
+            radius="md"
+          >
+            שמנו לב שכבר הכנת פגישה אחת או יותר -{' '}
+            <Link href="/scheduled">
+              <span className="font-bold hover:underline cursor-pointer">לצפייה</span>
+            </Link>
+          </Alert>
+        </div>
+      )}
     </>
   );
 };
