7c460d0c9562d361d97e2de14cbf61e5b4c8055a
Ofek Asido <asido.ofek@gmail.com>
2022-06-06 18:16:36 +0300

    Remove margin bottom

--- modules/eventId/EventInformation/Who.tsx
+++ modules/eventId/EventInformation/Who.tsx
@@ -6,23 +6,21 @@ const Who = ({ participantsData }) => {
     <>
       <h2 className="text-2xl font-medium text-right">מי?</h2>
       {participantsData.length > 0 ? (
-        <div className="mb-32">
-          <SimpleGrid cols={2} breakpoints={[{ maxWidth: 660, cols: 1 }]}>
-            {participantsData.map((participant, i) => {
-              return (
-                <div className="flex items-center my-1" key={i}>
-                  <div className="w-11 h-11 rounded-md bg-green-500 ml-2 flex justify-center items-center text-3xl text-white">
-                    <TiTick />
-                  </div>
-                  <div className="flex-1">
-                    <h3 className="font-medium">{participant.createdBy.name}</h3>
-                    <p>נרשמ/ה</p>
-                  </div>
+        <SimpleGrid cols={2} breakpoints={[{ maxWidth: 660, cols: 1 }]}>
+          {participantsData.map((participant, i) => {
+            return (
+              <div className="flex items-center my-1" key={i}>
+                <div className="w-11 h-11 rounded-md bg-green-500 ml-2 flex justify-center items-center text-3xl text-white">
+                  <TiTick />
                 </div>
-              );
-            })}
-          </SimpleGrid>
-        </div>
+                <div className="flex-1">
+                  <h3 className="font-medium">{participant.createdBy.name}</h3>
+                  <p>נרשמ/ה</p>
+                </div>
+              </div>
+            );
+          })}
+        </SimpleGrid>
       ) : (
         <p className="text-gray-500 text-right mb-5">אף אחד לא נרשם, בנתיים.</p>
       )}
