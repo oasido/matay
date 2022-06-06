571f361b80177cc2a246eb0a75ffe0eded9a0399
Ofek Asido <asido.ofek@gmail.com>
2022-06-06 18:16:46 +0300

    Change header styling

--- modules/eventId/EventInformation/Header.tsx
+++ modules/eventId/EventInformation/Header.tsx
@@ -6,26 +6,26 @@ const Header = (props) => {
 
   return (
     <>
-      <div className="bg-sky-500 rounded-md mx-2 my-2 pt-20 pb-4 text-center text-white">
-        <Title className="drop-shadow-lg">{eventData.title}</Title>
-        <Title className="drop-shadow-lg" order={5}>
+      <div className="bg-gray-700 rounded-md mx-2 my-2 pt-20 pb-4 text-center text-white">
+        <h1 className="text-3xl font-bold overflow-hidden">{eventData.title}</h1>
+        <h1 className="text-lg font-bold overflow-hidden">
           אורגן על ידי {eventData.createdBy.name}
-        </Title>
+        </h1>
         {eventData.location && (
           <div className="flex justify-center items-center text-sm">
             <GoLocation />
-            <span className="mr-1">{eventData.location}</span>
+            <span className="mr-1 overflow-hidden">{eventData.location}</span>
           </div>
         )}
       </div>
-      <div className="mx-10 mt-10">
+      {/* <div className="mx-10 mt-10">
         <Box className="bg-sky-500 p-4 rounded-md text-center text-white">
           <h3 className="text-2xl font-medium mb-5">הזמנה</h3>
           <p className="font-medium">
             נא ספק את הזמן המועדף עליך, כדי שהמארגן יוכל לבחור תאריך מתאים בהקדם האפשרי.
           </p>
         </Box>
-      </div>
+      </div> */}
     </>
   );
 };
