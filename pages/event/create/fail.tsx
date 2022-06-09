efa3be63a6db4f2c6019323b4955330236dbab1d
Ofek Asido <asido.ofek@gmail.com>
2022-06-09 03:05:09 +0300

    style: add alt text to image

--- pages/event/create/fail.tsx
+++ pages/event/create/fail.tsx
@@ -1,4 +1,3 @@
-import { Input, Button } from '@mantine/core';
 import Logo from '../../../modules/Logo';
 import Image from 'next/image';
 import FailImg from '../../../public/fail.png';
@@ -12,7 +11,7 @@ const Fail = () => {
         <h3 className="font-medium text-lg mb-5 text-gray-500">אירעה שגיאה, נסה שוב מאוחר יותר.</h3>
 
         <div className="mx-auto w-10/12 md:w-9/12 lg:w7/12 xl:w-6/12">
-          <Image src={FailImg} width={400} height={400} layout="fixed" />
+          <Image src={FailImg} width={400} height={400} layout="fixed" alt="failed image" />
         </div>
       </div>
     </>
